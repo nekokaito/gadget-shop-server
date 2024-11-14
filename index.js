05d210a814317e16b94a9bd281c23b39dd3d509b
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-14 17:18:40 +0600

    MongoDB setup done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
@@ -9,13 +10,31 @@ app.use(cors());
 app.use(express.json());
 
 //mongodb
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrlyepl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbConnect = async () => {
+  try {
+    client.connect();
+    console.log("DB Connected");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
+dbConnect();
 
 //api
-app.get ("/", (req,res) => {
-     res.send("server is running")
-})
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
-app.listen (port, ()=>{
-     console.log(`server is running on port, ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server is running on port, ${port}`);
+});
